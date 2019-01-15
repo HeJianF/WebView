899b361c022ac277c349ca110ed0f83ef0206ec3
HeJianFeng <1406423238@qq.com>
2019-01-15 15:22:40 +0800

    添加JsBridge

--- app/src/main/java/com/example/hjf/myh5test/FirstActivity.java
+++ app/src/main/java/com/example/hjf/myh5test/FirstActivity.java
@@ -16,11 +16,16 @@ public class FirstActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_main1:
+                //webView的用法
                 startActivity(new Intent(this, MainActivity.class));
                 break;
             case R.id.bt_main2:
+                //Android自带 js与native交互
                 startActivity(new Intent(this, JSActivity.class));
                 break;
+            case R.id.bt_main3:
+                //JSBridge
+                startActivity(new Intent(this, JsBridgeActivity.class));
             default:
         }
     }
--- app/src/main/java/com/example/hjf/myh5test/JSActivity.java
+++ app/src/main/java/com/example/hjf/myh5test/JSActivity.java
@@ -3,22 +3,19 @@ package com.example.hjf.myh5test;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Set;
 
 public class JSActivity extends AppCompatActivity {
@@ -36,7 +33,30 @@ public class JSActivity extends AppCompatActivity {
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         mWebView.loadUrl("file:///android_asset/javascript.html");
 
+        //Js调用Android 方法一，存在严重bug
+        mWebView.addJavascriptInterface(new AndroidToJs(), "test");
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //Js调用Android 方法二,对约定的url格式进行拦截检查，找到对应的进行想要的操作
+                Uri uri = Uri.parse(url);
+                if ("js".equals(uri.getScheme())) {
+                    if ("webView".equals(uri.getAuthority())) {
+                        Log.d("JSActivity", "shouldOverrideUrlLoading: js调用了Android的方法");
+                        Set<String> parameterNames = uri.getQueryParameterNames();
+                        for (String parameterName : parameterNames) {
+                            Log.d("JSActivity", parameterName + " = " + uri.getQueryParameter(parameterName));
+                        }
+                    }
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
         mWebView.setWebChromeClient(new WebChromeClient() {
+
             @Override
             public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
                 AlertDialog.Builder dialog = new AlertDialog.Builder(JSActivity.this);
@@ -52,38 +72,17 @@ public class JSActivity extends AppCompatActivity {
                 dialog.create().show();
                 return true;
             }
-        });
 
-        //Js调用Android方法
-        //方法一，存在严重bug
-        //mWebView.addJavascriptInterface(new AndroidToJs(), "test");
-        //方法二
-        mWebView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Uri uri = Uri.parse(url);
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                //Js调用Android 方法三，对url进行解析，找到对应的项目
+                Uri uri = Uri.parse(message);
                 if ("js".equals(uri.getScheme())) {
-                    if ("webView".equals(uri.getAuthority())) {
-                        Log.d("JSActivity", "shouldOverrideUrlLoading: js调用了Android的方法");
+                    if ("demo".equals(uri.getAuthority())) {
                         Set<String> parameterNames = uri.getQueryParameterNames();
                         for (String parameterName : parameterNames) {
                             Log.d("JSActivity", parameterName + " = " + uri.getQueryParameter(parameterName));
                         }
-                    }
-                    return true;
-                }
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-        });
-        //方法三
-        mWebView.setWebChromeClient(new WebChromeClient() {
-
-            @Override
-            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
-                Uri uri = Uri.parse(message);
-                if ("js".equals(uri.getScheme())) {
-                    if ("demo".equals(uri.getAuthority())) {
-                        Log.d("JSActivity", "onJsAlert: js调用了Android的方法");
                         result.confirm("js调用Android的方法成功了");
                     }
                     return true;
@@ -92,32 +91,29 @@ public class JSActivity extends AppCompatActivity {
             }
 
             @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                return super.onJsAlert(view, url, message, result);
-            }
-
-            @Override
             public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
                 return super.onJsConfirm(view, url, message, result);
             }
+
         });
+
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_android_js:
-                //方法一
-                mWebView.loadUrl("javascript:callJS()");
-                //方法二
-               /* mWebView.evaluateJavascript("javascript:callJS()", new ValueCallback<String>() {
+                //Android调用JS 方法一
+                //mWebView.loadUrl("javascript:callJS()");
+                //Android调用JS 方法二
+                mWebView.evaluateJavascript("javascript:callJS()", new ValueCallback<String>() {
                     @Override
                     public void onReceiveValue(String value) {
                         Log.d("JSActivity", "onReceiveValue: " + value);
                     }
-                });*/
+                });
                 break;
             case R.id.bt_js_android:
-                mWebView.loadUrl("javascript:returnResult(Android回传给js的值)");
+                mWebView.loadUrl("javascript:returnResult('Android回传给js的值')");
                 break;
             default:
         }
